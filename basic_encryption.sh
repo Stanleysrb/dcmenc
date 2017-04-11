3b47740005941e8a1e8e2ee7e24b1c88d87db44a
Stefan Mihajlovic <Stanleyserbia@gmail.com>
2017-04-11 02:09:20 +0200

    Added new options, placeholder for help, parametrized unique ID and encryption password which are required to do a study encrpytion with the same password

--- basic_encryption.sh
+++ basic_encryption.sh
@@ -6,11 +6,14 @@ export DCMDICTPATH=/usr/local/share/dcmtk/dicom.dic:/usr/local/share/dcmtk/priva
 
 # Get command options and process them:
 
-while getopts "f:ap:" arg; do
+while getopts "f:ap:h" arg; do
     case "$arg" in
         a ) TAGS=1;;
         f ) FILEPATH="${OPTARG}";;
 	p ) PRIVATE_TAG_BLOCK="${OPTARG}";;
+	e ) ENC_PASSWORD="${OPTARG}";;
+	u ) UNIQUE_ID="${OPTARG}";;
+	h ) echo "HELP TEXT GOES HERE, WILL BE DEFINED WHEN ALL OTHER THINGS ARE DONE"; exit 1;;
         -- ) ;;
         * ) if [ -z "$1" ]; then break; else echo "$1 is not a valid option"; exit 1; fi;;
     esac
@@ -26,7 +29,7 @@ else
 	fi
 fi
 
-#DYNAMIC TAGS, GET ALL TAGS FROM A DICOM FILE AND MAKE AN ARRAY
+#Dynamic tags, get all tags from a file except meta and pixel data, if parameter "-a" has been supplied, if not, standard set of tags is supplied by DICOM anonymization standard.
 
 if [ -z "$TAGS" ]; then
         TAGS=("0008,0014" "0008,0018" "0008,0050" "0008,0080" "0008,0081" "0008,0090" "0008,0092" "0008,0094" "0008,1010" "0008,1030" "0008,103E" "0008,1040" "0008,1048" "0008,1050" "0008,1060" "0008,1070" "0008,1080" "0008,1155" "0008,2111" "0010,0010" "0010,0020" "0010,0030" "0010,0032" "0010,0040" "0010,1000" "0010,1001" "0010,1010" "0010,1020" "0010,1030" "0010,1090" "0010,2160" "0010,2180" "0010,21B0" "0010,4000" "0018,1000" "0018,1030" "0020,000D" "0020,000E" "0020,0010" "0020,0052" "0020,0200" "0020,4000" "0040,0275" "0040,A124" "0040,A730" "0088,0140" "3006,0024" "3006,00C2")
@@ -37,10 +40,15 @@ if [ -z "$TAGS" ]; then
 	IFS=' ' read -r -a TAGS <<< $TEMP_TAGS
 fi
 
-# Generate Unique ID of a file and encryption password:
+# Generate Unique ID of a file and encryption password if not supplied manually:
 
-UNIQUE_ID=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
-ENC_PASSWORD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 128 | head -n 1`
+if [ -z "$UNIQUE_ID" ]; then
+	UNIQUE_ID=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
+fi
+
+if [ -z "$ENC_PASSWORD" ]; then
+	ENC_PASSWORD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 128 | head -n 1`
+fi
 
 # Check whether Private Tag block has been manually specified:
 
@@ -48,7 +56,6 @@ if [ -z "$PRIVATE_TAG_BLOCK" ]; then
 	PRIVATE_TAG_BLOCK="0909"
 fi
 
-#NEEDS TO BE DYNAMIC, TO HAVE A PARAMETERIZED PRIVATE_TAG_BLOCK
 PRIVATE_CREATOR=99
 FULL_CREATOR="$PRIVATE_TAG_BLOCK,00$PRIVATE_CREATOR"
 INCREMENT=1
@@ -71,6 +78,9 @@ NEW_TAGS=""
 dcmodify -i $FULL_CREATOR="DICOM_ENCRYPTION" "$FILEPATH"
 for TAG in "${TAGS[@]}"; do
 	HEX_INCREMENT=$( printf "%02x" $INCREMENT );
+
+# Check if tag is empty
+
 	DATA_EXISTS=`dcmdump +P "$TAG" "$FILEPATH"`
 	if [ -z "$DATA_EXISTS" ]; then
 		echo "TAG EMPTY $TAG"
