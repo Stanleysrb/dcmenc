187b582c7fef632d8567b9aa1c82b7ef78bbcbe5
Stefan Mihajlovic <Stanleyserbia@gmail.com>
2017-04-11 03:38:23 +0200

    Script for automated creation of private dictionary, if you want to use  custom private tag blocks

--- basic_decryption.sh
+++ basic_decryption.sh
@@ -1,25 +1,65 @@
 #!/bin/bash
 export DCMDICTPATH=/usr/local/share/dcmtk/dicom.dic:/usr/local/share/dcmtk/private.dic:/usr/local/share/dcmtk/diconde.dic:/home/dcmtk/resources/private.dic
-PRIVATE_TAG_BLOCK="0909"
+
+
+# Get command options and process them:
+
+while getopts "f:p:e:u:h" arg; do
+    case "$arg" in
+        f ) FILEPATH="${OPTARG}";;
+	p ) PRIVATE_TAG_BLOCK="${OPTARG}";;
+	e ) ENC_PASSWORD="${OPTARG}";;
+	u ) UNIQUE_ID="${OPTARG}";;
+	h ) echo "HELP TEXT GOES HERE, WILL BE DEFINED WHEN ALL OTHER THINGS ARE DONE"; exit 1;;
+        -- ) ;;
+        * ) if [ -z "$1" ]; then break; else echo "$1 is not a valid option"; exit 1; fi;;
+    esac
+done
+
+# Check whether file path has been specified, and whether file has extension ".dcm".
+
+if [ -z "$FILEPATH" ]; then 
+	echo "No DICOM file path specified. Exiting"; exit 1;	
+else
+	if [[ ${FILEPATH: -4} != ".dcm" ]]; then
+		echo "Not a .dcm file. Exiting"; exit 1;
+	fi
+fi
+
+# Check whether Private Tag block has been manually specified:
+
+if [ -z "$PRIVATE_TAG_BLOCK" ]; then
+	PRIVATE_TAG_BLOCK="0909"
+fi
+
 PRIVATE_CREATOR=99
 FULL_CREATOR="$PRIVATE_TAG_BLOCK,00$PRIVATE_CREATOR"
 PRIVATE_TAG_LOCATION="$PRIVATE_TAG_BLOCK,$PRIVATE_CREATOR"00""
 echo $PRIVATE_TAG_LOCATION
-TAGS=`dcmdump +P "$PRIVATE_TAG_LOCATION" "$1" | awk -F'[][]' '{print $2}'`
+LOCATOR_DATA=`dcmdump +L +P "$PRIVATE_TAG_LOCATION" "$FILEPATH" | awk -F'[][]' '{print $2}'`
+if [ -z "$LOCATOR_DATA" ]; then
+	echo "No Data in private tag block. Please manually specify if you have used custom Private block while encrypting. Exiting"; exit 1;
+fi
+UNIQUE_ID=`echo $LOCATOR_DATA | awk -F',' '{print $1}'`
+TAGS=`echo $LOCATOR_DATA | awk -F',' '{ st = index($0,",");print substr($0,st+1)}'`
+
+#SEARCH PASSWORD FILE FOR UNIQUE ID AND INJECT PASSWORD INTO VARIABLE
+
+
 ARRAY=($TAGS)
 echo $ARRAY
 for TAG in "${ARRAY[@]}"; do
         echo $TAG
-        DATA=`dcmdump +P "$TAG" "$1" | awk -F'[][]' '{print $2}'`
+        DATA=`dcmdump +L +P "$TAG" "$FILEPATH" | awk -F'[][]' '{print $2}'`
         ENCRYPTEDDATA=`echo $DATA | awk -F',' '{print $3}'`
         echo $DATA
         echo $ENCRYPTEDDATA
-        DECRYPTEDDATA=`echo $ENCRYPTEDDATA | openssl enc -d -base64 -aes-256-ctr -pass pass:stefan`
+        DECRYPTEDDATA=`echo $ENCRYPTEDDATA | openssl enc -d -base64 -aes-256-ctr -pass pass:Y9yfY9DM8gmYKcdgyHdEd6EAIxoHelUeYYs2MD1ypy5dX9V0QwTzIQ4pS2WHzAfB7d1KlofcReR0Si9jCxPUUbZKQrLhDstHhriGCpxYcZr7GRGhmCIneme1VQw3DWMo`
         FULL_TAG=`echo $DATA | awk -F',' '{print $1","$2}'`
         echo $FULL_TAG
-        dcmodify -m $FULL_TAG="$DECRYPTEDDATA" "$1"
-        dcmodify -e $TAG "$1"
+        dcmodify -m $FULL_TAG="$DECRYPTEDDATA" "$FILEPATH"
+        dcmodify -e $TAG "$FILEPATH"
 done
-dcmodify -e $FULL_CREATOR "$1"
-dcmodify -e $PRIVATE_TAG_LOCATION "$1"
+dcmodify -e $FULL_CREATOR "$FILEPATH"
+dcmodify -e $PRIVATE_TAG_LOCATION "$FILEPATH"
 
---
+++ create_private_dict.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while getopts "f:ap:e:u:h" arg; do
+    case "$arg" in
+        f ) FILEPATH="${OPTARG}";;
+        p ) PRIVATE_TAG_BLOCK="${OPTARG}";;
+        h ) echo "HELP TEXT GOES HERE, WILL BE DEFINED WHEN ALL OTHER THINGS ARE DONE"; exit 1;;
+        -- ) ;;
+        * ) if [ -z "$1" ]; then break; else echo "$1 is not a valid option"; exit 1; fi;;
+    esac
+done
+
+for i in {0..255}
+do
+	HEX_INCREMENT=$( printf "%02x" $i );
+	echo -e "($PRIVATE_TAG_BLOCK,\"DICOM_ENCRYPTION\",$HEX_INCREMENT)\tLO\tENCRYPTED\t1\tPrivateTag" >> $FILEPATH
+done
