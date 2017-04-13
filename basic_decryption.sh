a794480991b7c5fb2a89bd3f398630c584dbe924
Stefan Mihajlovic <Stanleyserbia@gmail.com>
2017-04-13 06:44:16 +0200

    Improved password file handling

--- basic_decryption.sh
+++ basic_decryption.sh
@@ -43,14 +43,21 @@ TAGS=`echo $LOCATOR_DATA | awk -F',' '{ st = index($0,",");print substr($0,st+1)
 
 # Check whether password file has been supplied, searching for unique ID, and setting ENC_PASSWORD.
 if [ ! -z "$PASSWORD_FILE" ]; then
-        echo "USING PASSWORD FILE";
+        echo "USING CUSTOM PASSWORD FILE";
+else	
+	echo "Using default password file"
+	PASSWORD_FILE="$KEYFILE"
+fi
+if [ -z "$KEYFILE" ]; then
+	echo "Keyfile variable not found in .bashrc"
+	exit 1;
+else
 	echo "Searching for the following UNIQUE_ID $UNIQUE_ID"
 	ENC_PASSWORD=`grep "$UNIQUE_ID" "$PASSWORD_FILE" | awk -F',' '{print $2}'`
 	if [ -z "$ENC_PASSWORD" ]; then
 		echo "Unable to find password for this file, please specify password manually"; exit 1;
 	fi
 fi
-
 # Put password file variable , possibly in variables file
 
 ARRAY=($TAGS)
