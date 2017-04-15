5bf144e78ce544fcca6d9d86d3061c820f0d6224
Stefan Mihajlovic <Stanleyserbia@gmail.com>
2017-04-15 07:31:02 +0200

    Added support for named nested (sequenced) tags

--- basic_encryption.sh
+++ basic_encryption.sh
@@ -35,7 +35,7 @@ echo $(($(date +%s%N)/1000000)) >> /home/smihajlovic/out.txt
 #Dynamic tags, get all tags from a file except meta and pixel data, if parameter "-a" has been supplied, if not, standard set of tags is supplied by DICOM anonymization standard.
 
 if [ -z "$TAGS" ]; then
-        TAGS=("0008,0014" "0008,0018" "0008,0050" "0008,0080" "0008,0081" "0008,0090" "0008,0092" "0008,0094" "0008,1010" "0008,1030" "0008,103E" "0008,1040" "0008,1048" "0008,1050" "0008,1060" "0008,1070" "0008,1080" "0008,1155" "0008,2111" "0010,0010" "0010,0020" "0010,0030" "0010,0032" "0010,0040" "0010,1000" "0010,1001" "0010,1010" "0010,1020" "0010,1030" "0010,1090" "0010,2160" "0010,2180" "0010,21B0" "0010,4000" "0018,1000" "0018,1030" "0020,000D" "0020,000E" "0020,0010" "0020,0052" "0020,0200" "0020,4000" "0040,0275" "0040,A124" "0040,A730" "0088,0140" "3006,0024" "3006,00C2")
+        TAGS=("0008,0014" "0008,0018" "0008,0050" "0008,0080" "0008,0081" "0008,0090" "0008,0092" "0008,0094" "0008,1010" "0008,1030" "0008,103E" "0008,1040" "0008,1048" "0008,1050" "0008,1060" "0008,1070" "0008,1080" "0008,1155" "0008,2111" "0010,0010" "0010,0020" "0010,0030" "0010,0032" "0010,0040" "0010,1000" "0010,1001" "0010,1010" "0010,1020" "0010,1030" "0010,1090" "0010,2160" "0010,2180" "0010,21B0" "0010,4000" "0018,1000" "0018,1030" "0020,000D" "0020,000E" "0020,0010" "0020,0052" "0020,0200" "0020,4000" "0040,A124" "0040,A730" "0088,0140" "3006,0024" "3006,00C2" "0040,0275.0032,1064.0008,0100" )
 	else
 	LINE_NUM=`dcmdump "$FILEPATH" | grep -E '(7fe0,0010)' -i -n | awk -F':' '{print $1}'`
 	LINE_NUM=$((LINE_NUM-1))
@@ -60,6 +60,7 @@ if [ -z "$PRIVATE_TAG_BLOCK" ]; then
 	PRIVATE_TAG_BLOCK="0909"
 fi
 
+
 PRIVATE_CREATOR=99
 FULL_CREATOR="$PRIVATE_TAG_BLOCK,00$PRIVATE_CREATOR"
 INCREMENT=1
@@ -79,9 +80,26 @@ NEW_TAGS_DATA=""
 dcmodify -i $FULL_CREATOR="DICOM_ENCRYPTION" "$FILEPATH"
 for TAG in "${TAGS[@]}"; do
 	HEX_INCREMENT=$( printf "%02x" $INCREMENT );
-
+	if [ ${#TAG} != 9 ]; then
+		echo "TAG" $TAG "is LONG"
+		LONG_TAG=$TAG
+		LONG_TAG_CHECKER=`echo $TAG | awk '{ gsub("\\\.",".*"); print $0}'`
+		echo $LONG_TAG_CHECKER
+		CUTTER=${#TAG}
+		CUTTER=`expr $CUTTER - 8`
+		echo $CUTTER
+		TAG=`echo $TAG | cut -c $CUTTER-`
+		DATA_EXISTS=`dcmdump +L +P "$TAG" +p "$FILEPATH" | grep "$LONG_TAG_CHECKER"`
+		echo $DATA_EXISTS
+                #REVERT TAG TO LONG VALUE
+                TAG=`echo $LONG_TAG | awk '{ gsub("\\\.",")[0].("); print $0}'`
+                TAG="("$TAG")"
+                echo "LONG TAG IS NOW:" $TAG
+	else
+		echo "TAG" $TAG "is short"
+		DATA_EXISTS=`dcmdump +L +P "$TAG" "$FILEPATH"`
+	fi
 # Check if tag is empty
-	DATA_EXISTS=`dcmdump +L +P "$TAG" "$FILEPATH"`
 	if [ -z "$DATA_EXISTS" ]; then
 		echo "TAG EMPTY $TAG"
 	        echo "encryption skip" $(($(date +%s%N)/1000000)) >> /home/smihajlovic/out.txt
@@ -92,6 +110,7 @@ for TAG in "${TAGS[@]}"; do
 	if [ -z "$DATA" ]; then
 		DATA=`dcm2xml "$FILEPATH" | grep "tag=\"${TAG}\"" | awk  'BEGIN {RS="<[^>]+>"} {print $0}' | openssl enc -e -base64 -A -aes-256-ctr -pass pass:$ENC_PASSWORD`
 	else
+#CAN BE SLIGHTLY SHORTER!!!
 		DATA=`echo $DATA_EXISTS | awk -F'[][]' '{print $2}' | openssl enc -e -base64 -A -aes-256-ctr -pass pass:$ENC_PASSWORD`
 	fi
 	echo "TAG $TAG OK!!!"
@@ -112,7 +131,8 @@ for TAG in "${TAGS[@]}"; do
 done
 
 echo "REMOVING OLD TAG DATA"
-dcmodify $OLD_TAGS "$FILEPATH"
+dcmodify "$FILEPATH" $OLD_TAGS -v
+echo "$OLD_TAGS" \'$FILEPATH\'
 #echo "dcmodify" $NEW_TAG_DATA "$FILEPATH"
 echo "Inserting new values in new tags"
 dcmodify $NEW_TAGS_DATA "$FILEPATH"
@@ -120,6 +140,7 @@ dcmodify $NEW_TAGS_DATA "$FILEPATH"
 FULL_TAG=$(($PRIVATE_CREATOR*100))
 FULL_TAG="$PRIVATE_TAG_BLOCK,$FULL_TAG"
 DESCRIPTOR_TAG="$UNIQUE_ID,$NEW_TAGS"
+echo "dcmodify -i" "$FULL_TAG=""$DESCRIPTOR_TAG" "$FILEPATH"
 dcmodify -i $FULL_TAG="$DESCRIPTOR_TAG" "$FILEPATH"
 
 #echo "BUILT DESCRIPTOR TAG: $DESCRIPTOR_TAG"
