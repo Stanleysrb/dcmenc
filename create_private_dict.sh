09b6bb4b1167bc89a911e38ad190e1f0d8ea418d
Stefan Mihajlovic <stanleyserbia@gmail.com>
2017-08-18 00:15:30 +0200

    minor tweaks

--- create_private_dict.sh
+++ create_private_dict.sh
@@ -4,7 +4,7 @@ while getopts "f:ap:e:u:h" arg; do
     case "$arg" in
         f ) FILEPATH="${OPTARG}";;
         p ) PRIVATE_TAG_BLOCK="${OPTARG}";;
-        h ) echo "HELP TEXT GOES HERE, WILL BE DEFINED WHEN ALL OTHER THINGS ARE DONE"; exit 1;;
+        h ) echo "The following parameters are supported: -f FILEPATH (Location of DICOM private dictionary file) -p PRIVATE_TAG_BLOCK (Custom Private Tag Block)"; exit 1;;
         -- ) ;;
         * ) if [ -z "$1" ]; then break; else echo "$1 is not a valid option"; exit 1; fi;;
     esac
@@ -16,5 +16,3 @@ do
 	echo -e "($PRIVATE_TAG_BLOCK,\"DICOM_ENCRYPTION\",$HEX_INCREMENT)\tLO\tENCRYPTED\t1\tPrivateTag" >> $FILEPATH
 done
 
-
-# STOP IF ITS NOT PRIVATE TAG BLOCK
